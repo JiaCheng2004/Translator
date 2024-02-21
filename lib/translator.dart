019751d57985eb64d399bedd04938dedb45a4757
Simon Zhao <68301003+SimonZhao7@users.noreply.github.com>
2024-02-21 00:08:34 -0800

    Improve audio quality by a significant amount.
    Rescaled 16-bit signed integers to value between [-1, 1] and reduced volume of audio data

--- lib/translator.dart
+++ lib/translator.dart
@@ -14,7 +14,12 @@ class IllegalArgumentException implements Exception {
 }
 
 class Translator {
+  static const pcm16BitMax = 32767;
+  static const pcm16BitMin = pcm16BitMax + 1;
+  static const reductionRatio = 0.7;
   static const recorderSettings = RecordConfig(encoder: AudioEncoder.pcm16bits);
+  static const recordingFileName = 'translator_data.wav';
+
   final recorder = AudioRecorder();
   StreamSubscription<Uint8List>? subscription;
   List<double> leftBuffer = [];
@@ -41,11 +46,14 @@ class Translator {
     final recordStream = await recorder.startStream(recorderSettings);
     subscription = recordStream.listen((bytes) {
       final signedPCMData = recorder.convertBytesToInt16(bytes);
+
       for (int i = 0; i < signedPCMData.length; i++) {
+        double val = _reduceSound(_rescale(signedPCMData[i]));
+
         if (i % 2 == 0) {
-          leftBuffer.add(signedPCMData[i].toDouble());
+          leftBuffer.add(val);
         } else {
-          rightBuffer.add(signedPCMData[i].toDouble());
+          rightBuffer.add(val);
         }
       }
     });
@@ -54,8 +62,7 @@ class Translator {
   void pauseRecording() async {
     await recorder.pause();
     final directory = await getApplicationCacheDirectory();
-    final file = File('${directory.path}/translator_data.wav');
-
+    final file = File('${directory.path}/$recordingFileName');
     final wav = Wav(
       [
         Float64List.fromList(leftBuffer),
@@ -92,5 +99,16 @@ class Translator {
     final player = AudioPlayer();
     await player.setFilePath(file.path);
     await player.play();
+  }
+
+  double _rescale(int val) {
+    if (val > 0) {
+      return val / pcm16BitMax;
+    }
+    return val / pcm16BitMin;
+  }
+
+  double _reduceSound(double val) {
+    return reductionRatio * val;
   }
 }
